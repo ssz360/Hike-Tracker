ff80b44bfb5f8af9d7b60c2000891546b0346f94
SSZ <barg3abz@gmail.com>
2022-11-10 19:11:15 +0100

    install and config Cypress, add test for mainPage

---
+++ client/cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      config.excludeSpecPattern = ['*/**/2-advanced-examples','*/**/1-getting-started']
+      // implement node event listeners here
+      config.baseUrl = "http://localhost:3000/";
+
+      return config;
+    },
+  }
+})
---
+++ client/cypress/e2e/integration-tests/main-page.cy.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+
+describe('select listboxes and click search', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it("if all the listboxes' default options are selected at first", () => {
+        cy.get("select").each((el, index, list) => {
+            el[0].selectedIndex = 1;
+        });
+
+        cy.get("button[type='submit']").click();
+    });
+})
