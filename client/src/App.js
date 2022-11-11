c751a5929c71719c8dcb8062c443753c56eb20d7
SSZ <barg3abz@gmail.com>
2022-11-11 14:22:17 +0100

    add route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
-import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   const [logged,setLogged]=useState(false);
 
   return (
     <>
-      <Routes>
-        <Route path='/' element={<h1>Hike Tracker!</h1>} />
-      </Routes>
+      <Container>
+        <Row>
+          <Col>
+            <Routes>
+              <Route path='/hiker-hikes'/>
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
