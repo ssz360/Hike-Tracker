44c316a786eab7bbdfd7768a8188328df32f06bc
mell0r1ne <s303330@studenti.polito.it>
2022-11-14 09:07:22 +0100

    Cypress testing

---
+++ client/cypress/e2e/testing.cy.js
@@ -0,0 +1,14 @@
+
+/*describe("Get Hikes", () => {
+	it("Test", () => {
+		cy.reload();
+		cy.get("input[placeholder=Name]").type("Good hut");
+		cy.get("input[placeholder=Country]").type("Italy");
+		cy.get("input[placeholder=NumOfGuest]").type("10");
+		cy.get("input[placeholder=NumOfRooms]").type("10");
+		cy.get("input[placeholder=Coordinates]").type("1,1");
+		cy.contains("Save").click();
+		cy.url().should("eq", "http://localhost:3000/");
+	});
+});
+*/
