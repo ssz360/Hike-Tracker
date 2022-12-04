1abf8aaab5b0eb979682dafd4032fcd7404f78a5
Andrea Fois <s295102@studenti.polito.it>
2022-12-04 12:57:35 +0100

    Restyling SignUp page

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import { Form, Button, Alert } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import React from 'react';
 import api from "../lib/api";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import { Row, Col } from 'react-bootstrap';
 
 
 function SignUp(props) {
@@ -18,6 +17,8 @@ function SignUp(props) {
     const [password2, setPassword2] = useState('');
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
+
+    const [isHover, setIsHover] = useState(false);
 
     var emailValidator = require("node-email-validation");
 
@@ -34,24 +35,39 @@ function SignUp(props) {
 
 
     return (
-        <Col>
+    <div style={{ 
+        backgroundImage: "url(./images/bg_signup.jpg)",
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+    }}>
+      <Container fluid>          
             <Row>
-                <br></br><br></br>
+            <div className="d-flex align-items-center justify-content-center text-center not-found-container">
+            <h3 className="mt-3" 
+                style={{fontFamily:"Montserrat,Helvetica,Arial,Lucida,sans-serif", 
+                fontWeight: "800",
+                fontSize: "49px",
+                color: "#0d0d0d",
+                textShadow: "2px 2px 4px #cccccc"}}>Welcome on board!</h3>
+            </div>
+            <div className="d-flex align-items-center justify-content-center text-center not-found-container">
+            <h5 style={{fontFamily:"Montserrat,Helvetica,Arial,Lucida,sans-serif", 
+                fontWeight: "750",
+                fontSize: "20px",
+                color: "#0d0d0d",
+                textShadow: "2px 2px 4px #cccccc"}}>Please fill the form to get the best experience from our app</h5>
+            </div>
             </Row>
-            <h3>Welcome on board!</h3>
-            <h5>Please fill the form to get the best experience from our app</h5>
-            <Row>
-                <Col>
-                    <Form style={
-                        {
-                            "margin": 0,
-                            "position": "absolute",
-                            "top": "50%",
-                            "left": "50%",
-                            "msTransform": "translate(-50%, -50%)",
-                            "transform": "translate(-50%, -50%)"
-                        }
-                    } onSubmit={async e => {
+            <Row className="mt-3">
+            <div className="d-flex align-items-center justify-content-center text-center not-found-container"
+                style={{
+                    opacity:"90%"
+                }}>
+                    <Form className="shadow-lg p-3 mb-5 bg-white rounded" 
+                        style={{
+                            width:"22%"
+                        }}
+                        onSubmit={async e => {
                         e.preventDefault();
                         e.stopPropagation();
 
@@ -131,11 +147,18 @@ function SignUp(props) {
 
                         {error != '' ? <Alert className="my-3" variant="danger">{error}</Alert> : <></>}
                         {success != '' ? <Alert className="my-3" variant="success">{error}</Alert> : <></>}
-                            <Button style={{ justifyContent: 'right'}} variant="secondary" type="submit">Submit</Button>
+                            <Button type="submit" 
+                                style={{ 
+                                    justifyContent: 'right',
+                                    backgroundColor: !isHover ? '#009999' : '#00cccc',
+                                    borderColor: "white"
+                                }}
+                                onMouseEnter={ () => setIsHover(true) }
+                                onMouseLeave={ () => setIsHover(false) }>Submit</Button>
                     </Form>
-                </Col>
-            </Row>
-        </Col>
+                    </div>
+                    </Row>
+        </Container></div>
     );
 }
 
