fb64fe47234ec6fc7206126aca27aeb0b3c560f0
Moyne <Moyne11@outlook.it>
2023-01-09 00:35:08 +0100

    huts and parking api test fixes

--- server/test/huts.js
+++ server/test/huts.js
@@ -1,6 +1,7 @@
 const { should, expect } = require('chai');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const fs= require('fs');
 const { afterEach, beforeEach, it } = require('mocha');
 chai.use(chaiHttp);
 chai.should();
@@ -14,42 +15,57 @@ describe('filtering huts apis', () => {
     it('no filters',done=>{
         agent.post('/api/huts/list').then(res=>{
             res.should.have.status(200);
-            expect(res.body.length).equal(7);
+            expect(res.body.length).equal(1);
             done();
         })
     })
     
-    it('Rifugio filters',done=>{
-        const filters={ name:"Rifugio", country:null,
+    it('Rocciamelone filters',done=>{
+        const filters={ name:"Rocciamelone", country:null,
          numberOfBedrooms:null, 
          coordinate:null, geographicalArea:null }
         agent.post('/api/huts/list').send(filters).then(res=>{
             res.should.have.status(200);
-            expect(res.body.length).equal(4);
+            expect(res.body.length).equal(1);
             done();
         })
     })
 
     it('Add new hut',done=>{
         const hut={
-                 name:"first hut",
-                 country:"italy",
-                 numberOfGuests:5,
-                 numberOfBedrooms:4,
-                 coordinate:"41.000144, 14.534893"
-              }
-        agent.post('/api/huts').send(hut).then(res=>{
-            console.log("Res status",res.status);
-            const filters={ name:"first hut", country:null,
-          numberOfBedrooms:null, 
-         coordinate:null, geographicalArea:null }
-        agent.post('/api/huts/list').send(filters).then(res=>{
-            console.log("Res.status",res.status);
-            console.log("Res body len",res.body.length);
-            res.should.have.status(200);
-            expect(res.body.length).equal(1);
-            done();
-        })
+            name:"first hut",
+            email:"testhut@gmail.com",
+            description:"first test hut description",
+            numberOfBedrooms:4,
+            latitude:41.000144,
+            longitude: 14.534893,
+            phone:'3933009911',
+            website:undefined
+        }
+        const imagehike=fs.readFileSync(`${__dirname}/../tmp/hike.jpg`);
+        agent.post('/api/login').send({username:"davidwallace@gmail.com",password:"123abcABC!"}).then(res=>{
+            agent.post('/api/huts').set('content-type', 'multipart/form-data')
+                .field('name', hut.name)
+                .field('description', hut.description)
+                .field('email', hut.email)
+                .field('numberOfBedrooms',hut.numberOfBedrooms)
+                .field('latitude',hut.latitude)
+                .field('longitude',hut.longitude)
+                .field('phone',hut.phone)
+                .attach('images', imagehike, 'tests/hike.jpg')
+                .then(res=>{
+                    console.log("Res status",res.status);
+                    const filters={ name:"first hut", country:null,
+                        numberOfBedrooms:null, 
+                        coordinate:null, geographicalArea:null }
+                    agent.post('/api/huts/list').send(filters).then(res=>{
+                        console.log("Res.status",res.status);
+                        console.log("Res body len",res.body.length);
+                        res.should.have.status(200);
+                        expect(res.body.length).equal(1);
+                        done();
+                })
+            })
         })
     })
 
--- server/test/parking.js
+++ server/test/parking.js
@@ -12,26 +12,32 @@ var agent = chai.request.agent(app);
 describe('filtering huts apis', () => {
 
     it('get parking list',done=>{
-        agent.get('/api/parkings').then(res=>{
-            res.should.have.status(200);
-            expect(res.body.length).equal(4);
-            done();
-        })
+        agent.post('/api/login').send({username:"davidwallace@gmail.com",password:"123abcABC!"}).then(res=>{
+            agent.get('/api/parkings').then(res=>{
+                console.log('get status',res.status,'body',res.body);
+                res.should.have.status(201);
+                expect(res.body.length).equal(1);
+                done();
+            })
+        });
     })
     
     it('new parking',done=>{
         const pk = {
              name:"Test parking",
             desc:"Test for parking it",
-            coordinates:[1,1],
-            geographicalArea:"Italy",
+            coordinates:[ 45.27, 7.62 ],
             slots:4
            };
-        agent.post('/api/parking').send(pk).then(res=>{
-            agent.get('/api/parkings').then(res=>{
-                res.should.have.status(200);
-                expect(res.body.length).equal(5);
-                done();
+        agent.post('/api/login').send({username:"davidwallace@gmail.com",password:"123abcABC!"}).then(res=>{
+            agent.post('/api/parking').send(pk).then(res=>{
+                console.log('post status',res.status,'body',res.body);
+                agent.get('/api/parkings').then(res=>{
+                    console.log('get status',res.status,'body',res.body);
+                    res.should.have.status(201);
+                    expect(res.body.length).equal(2);
+                    done();
+                })
             })
         })
     })
